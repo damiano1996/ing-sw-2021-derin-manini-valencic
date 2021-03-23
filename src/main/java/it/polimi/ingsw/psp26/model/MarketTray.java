fb4edf43a3569413ed6ec7cad1e1ca6036c3ebc0
JasV13 <JasV1113@gmail.com>
2021-03-23 17:07:41 +0100

    Implemented and tested MarketTray

--- src/main/java/it/polimi/ingsw/psp26/model/MarketTray.java
+++ src/main/java/it/polimi/ingsw/psp26/model/MarketTray.java
@@ -1,51 +1,64 @@
 package it.polimi.ingsw.psp26.model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+
 public class MarketTray {
 
     private final Resource[][] marketMarbles;
     private Resource marbleOnSlide;
 
-
     public MarketTray() {
-        marketMarbles = new Resource[3][4];
+    List<Resource> MarblesList = new ArrayList<Resource>(Arrays.asList(Resource.EMPTY,Resource.EMPTY,Resource.EMPTY,Resource.EMPTY,
+            Resource.COIN, Resource.COIN,Resource.SERVANT,Resource.SERVANT,Resource.SHIELD, Resource.SHIELD, Resource.STONE,
+            Resource.STONE, Resource.FAITH_MARKERS));
+    Collections.shuffle(MarblesList);
+
+    marketMarbles = new Resource[3][4];
+    marbleOnSlide = MarblesList.get(MarblesList.size() - 1);
+    for(int i = 0; i <  marketMarbles[0].length; i++){
+        for(int j = 0; j < marketMarbles.length; j++){
+            marketMarbles[j][i] = MarblesList.get(j + i * marketMarbles.length);
+        }
+    }
     }
 
 
-    /**
-     * Used in constructor
-     */
-    private void shuffle() {
-
-
-    }
-
-
-    public Resource[] getMarblesOnRow(int row) {
+    public Resource[] getMarblesOnRow(int row){
         return marketMarbles[row];
     }
 
 
     public Resource[] getMarbleOnColumn(int column) {
-        Resource[] marketColumn = new Resource[3];
-        for (int i = 0; i < 3; i++) {
-            marketColumn[i] = marketMarbles[i][column]; //maybe a single marble getter is required
+        Resource[] marketColumn = new Resource[marketMarbles.length];
+        for (int i = 0; i < marketMarbles.length; i++) {
+            marketColumn[i] = marketMarbles[i][column];
         }
         return marketColumn;
-
     }
 
 
-    public Resource getMarbleOnSlide() {
-        return marbleOnSlide;
-    }
+    public Resource getMarbleOnSlide() { return marbleOnSlide; }
 
 
     public void pushMarbleFromSlideToRow(int row) {
 
+        Resource marbleTemp = marbleOnSlide;
+        marbleOnSlide = marketMarbles[row][marketMarbles[0].length - 1];
+        for(int i = 0; i < marketMarbles[0].length - 1; i++){
+            marketMarbles[row][marketMarbles[0].length - i - 1] = marketMarbles[row][marketMarbles[0].length - i - 2];
+        }
+        marketMarbles[row][0] = marbleTemp;
     }
 
 
     public void pushMarbleFromSlideToColumn(int column) {
-
+        Resource marbleTemp = marbleOnSlide;
+        marbleOnSlide = marketMarbles[marketMarbles.length - 1][column];
+        for(int i = 0; i < marketMarbles.length - 1; i++) marketMarbles[marketMarbles.length - i - 1][column] = marketMarbles[ marketMarbles.length - i - 2][column];
+        marketMarbles[0][column] = marbleTemp;
     }
 }
---
+++ src/test/java/it/polimi/ingsw/psp26/model/MarketTrayTest.java
@@ -0,0 +1,71 @@
+package it.polimi.ingsw.psp26.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class MarketTrayTest {
+
+    private MarketTray Market;
+
+    @Test
+    public void testGetMarblesOnRow() {
+        Market = new MarketTray();
+        Market.getMarblesOnRow(0);
+    }
+
+    @Test
+    public void testGetMarbleOnColumn() {
+        Market = new MarketTray();
+        Market.getMarbleOnColumn(0);
+    }
+
+    @Test
+    public void testGetMarbleOnSlide() {
+        Market = new MarketTray();
+        Market.getMarbleOnSlide();
+    }
+
+    @Test
+    public void testPushMarbleFromSlideToRow_TrueCase() {
+        Market = new MarketTray();
+        Resource[] MarketRow = Market.getMarblesOnRow(0).clone();
+        for(int i = 0; i < MarketRow.length + 1; i ++) {
+            Market.pushMarbleFromSlideToRow(0);
+        }
+        assertArrayEquals(MarketRow, Market.getMarblesOnRow(0));
+    }
+
+    @Test
+    public void testPushMarbleFromSlideToRow_FalseCase() {
+        Market = new MarketTray();
+        Resource[] MarketRow = Market.getMarblesOnRow(0).clone();
+        for(int i = 0; i < MarketRow.length; i ++) {
+            Market.pushMarbleFromSlideToRow(0);
+        }
+        assertFalse(Arrays.equals(MarketRow, Market.getMarblesOnRow(0)));
+    }
+
+    @Test
+    public void testPushMarbleFromSlideToColumn_TrueCase() {
+        Market = new MarketTray();
+        Resource[] MarketColumn = Market.getMarbleOnColumn(0).clone();
+        for(int i = 0; i < MarketColumn.length + 1; i ++) {
+            Market.pushMarbleFromSlideToColumn(0);
+        }
+        assertArrayEquals(MarketColumn, Market.getMarbleOnColumn(0));
+    }
+
+    @Test
+    public void testPushMarbleFromSlideToColumn_FalseCase() {
+        Market = new MarketTray();
+        Resource[] MarketColumn = Market.getMarbleOnColumn(0).clone();
+        for(int i = 0; i < MarketColumn.length; i ++) {
+            Market.pushMarbleFromSlideToColumn(0);
+        }
+        assertFalse(Arrays.equals(MarketColumn, Market.getMarbleOnColumn(0)));
+    }
+}

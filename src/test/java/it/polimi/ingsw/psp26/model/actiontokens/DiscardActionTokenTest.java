dd3f5e410d73d25deb5036c1fb5cd42fe95702f3
andreamanini98 <andrea3.manini@mail.polimi.it>
2021-04-01 22:28:30 +0200

    Refactored PersonalBoardTest's classes code, now more ObjectOriented Fixed Depot exception throwing in addResource(List<Resource> newResources) method Added equals() method in VaticanReportSection Reduced DiscardActionTokenTest code by adding for loops

--- src/test/java/it/polimi/ingsw/psp26/model/actiontokens/DiscardActionTokenTest.java
+++ src/test/java/it/polimi/ingsw/psp26/model/actiontokens/DiscardActionTokenTest.java
@@ -31,165 +31,99 @@ public class DiscardActionTokenTest {
     @Test
     public void testExecute_TwoCardsInFirstLevelGreen_NoExceptionThrowing() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 2; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
     }
 
     //Token takes 2 cards from first level. User tries to draw 3 cards from first level. NoMoreDevelopmentCardException rised.
     @Test(expected = NoMoreDevelopmentCardsException.class)
     public void testExecute_TwoCardsInFirstLevelGreen_NoMoreDevelopmentCardsException() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
     }
 
     //Token takes 1 card from first level and 1 card from second level. User draws 3 cards from second level. No exception rised.
     @Test
     public void testExecute_OneCardFirstLevelOneCardSecondLevelGreen_NoExceptionThrowing() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
     }
 
     //Token takes 1 card from first level and 1 card from second level. User tries to draw 4 cards from second level. NoMoreDevelopmentCardsException rised.
     @Test(expected = NoMoreDevelopmentCardsException.class)
     public void testExecute_OneCardFirstLevelOneCardSecondLevelGreen_NoMoreDevelopmentCardsException() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
     }
 
     //Token takes 2 card from second level. User draws 2 cards from second level. No exception rised.
     @Test
     public void testExecute_TwoCardsInSecondLevelGreen_NoExceptionThrowing() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 2; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
     }
 
     //Token takes 2 card from second level. User tries to draw 3 cards from second level. NoMoreDevelopmentCardsException rised.
     @Test(expected = NoMoreDevelopmentCardsException.class)
     public void testExecute_TwoCardsInSecondLevelGreen_NoMoreDevelopmentCardsException() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
     }
 
     //Token takes 1 card from second level and 1 card from third level. User draws 3 cards from third level. No exception rised.
     @Test
     public void testExecute_OneCardSecondLevelOneCardThirdLevelGreen_NoExceptionThrowing() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.THIRD);
     }
 
     //Token takes 1 card from second level and 1 card from third level. User tries to draw 4 cards from third level. NoMoreDevelopmentCardsException rised.
     @Test(expected = NoMoreDevelopmentCardsException.class)
     public void testExecute_OneCardSecondLevelOneCardThirdLevelGreen_NoMoreDevelopmentCardsException() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.THIRD);
     }
 
     //Token takes 2 card from third level. User draws 2 cards from third level. No exception rised.
     @Test
     public void testExecute_TwoCardsInThirdLevelGreen_NoExceptionThrowing() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
+        for (int i = 0; i < 2; i++) developmentGrid.drawCard(Color.GREEN, Level.THIRD);
     }
 
     //Token takes 2 card from third level. User tries to draw 3 cards from third level. NoMoreDevelopmentCardsException rised.
     @Test(expected = NoMoreDevelopmentCardsException.class)
     public void testExecute_TwoCardsInThirdLevelGreen_NoMoreDevelopmentCardsException() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.THIRD);
     }
 
     //Token takes all cards from third level.
     @Test(expected = LorenzoWinException.class)
     public void testExecute_EmptyColumnGreen_LorenzoWinException_OneCardLeft() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 3; i++) developmentGrid.drawCard(Color.GREEN, Level.THIRD);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
     }
 
     //Token takes all cards from third level.
     @Test(expected = LorenzoWinException.class)
     public void testExecute_EmptyColumnGreen_LorenzoWinException_TwoCardsLeft() throws NoMoreDevelopmentCardsException, LevelDoesNotExistException, ColorDoesNotExistException, LorenzoWinException {
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.FIRST);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.SECOND);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
-        developmentGrid.drawCard(Color.GREEN, Level.THIRD);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.FIRST);
+        for (int i = 0; i < 4; i++) developmentGrid.drawCard(Color.GREEN, Level.SECOND);
+        for (int i = 0; i < 2; i++) developmentGrid.drawCard(Color.GREEN, Level.THIRD);
         discardActionTokenGreen.execute(faithTrack, developmentGrid);
     }
 

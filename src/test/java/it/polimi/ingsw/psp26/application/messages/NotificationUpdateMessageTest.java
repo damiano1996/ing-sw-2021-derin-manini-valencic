5352e195b7f0e025c96b00cee45d6719d9f9a24d
damiano9669 <damiano.derin@gmail.com>
2021-05-07 16:20:16 +0200

    Introduction of model live update.
    New CachedModel class to keep track of updates.

--- src/test/java/it/polimi/ingsw/psp26/application/messages/NotificationUpdateMessageTest.java
+++ src/test/java/it/polimi/ingsw/psp26/application/messages/NotificationUpdateMessageTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class LiveUpdateMessageTest {
+public class NotificationUpdateMessageTest {
 
     @Test
     public void testMessagePayloadExtraction() throws InvalidPayloadException, EmptyPayloadException {

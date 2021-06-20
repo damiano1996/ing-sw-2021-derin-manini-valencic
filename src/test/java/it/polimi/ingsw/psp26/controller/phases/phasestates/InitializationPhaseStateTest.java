689338ed1b01756480e6bc3695438afd0fc6ecaf
andreamanini98 <andrea3.manini@mail.polimi.it>
2021-06-20 22:05:05 +0200

    Fixed InitializationPhaseStateTest

--- src/test/java/it/polimi/ingsw/psp26/controller/phases/phasestates/InitializationPhaseStateTest.java
+++ src/test/java/it/polimi/ingsw/psp26/controller/phases/phasestates/InitializationPhaseStateTest.java
@@ -1,14 +1,19 @@
 package it.polimi.ingsw.psp26.controller.phases.phasestates;
 
+import it.polimi.ingsw.psp26.application.files.Files;
 import it.polimi.ingsw.psp26.application.messages.MessageType;
 import it.polimi.ingsw.psp26.application.messages.SessionMessage;
 import it.polimi.ingsw.psp26.controller.MatchController;
 import it.polimi.ingsw.psp26.controller.phases.Phase;
 import it.polimi.ingsw.psp26.exceptions.InvalidPayloadException;
 import it.polimi.ingsw.psp26.network.server.VirtualView;
+import it.polimi.ingsw.psp26.network.server.memory.GameSaver;
+import it.polimi.ingsw.psp26.network.server.memory.Users;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import static it.polimi.ingsw.psp26.configurations.Configurations.GAME_FILES;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
@@ -25,7 +30,11 @@ public class InitializationPhaseStateTest {
     @Test
     public void testAddPlayer() throws InvalidPayloadException {
         String nickname = "nickname";
+        String password = "password";
         String sessionToken = "sessionToken";
+
+        Users.getInstance().addUser(nickname, password, sessionToken);
+
         phase.execute(new SessionMessage(
                 sessionToken,
                 MessageType.ADD_PLAYER,
@@ -34,6 +43,13 @@ public class InitializationPhaseStateTest {
 
         assertEquals(phase.getMatchController().getMatch().getPlayers().get(0).getNickname(), nickname);
         assertEquals(phase.getMatchController().getMatch().getPlayers().get(0).getSessionToken(), sessionToken);
+    }
+
+    @After
+    public void tearDown() {
+        GameSaver.getInstance().deleteDirectoryByMatchId(phase.getMatchController().getMatch().getId());
+        Files.deleteFile(GAME_FILES + "nickname-password.json");
+        Files.deleteFile(GAME_FILES + "nickname-sessionToken.json");
     }
 
     @Test
